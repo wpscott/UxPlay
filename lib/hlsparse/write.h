3478213a8f99b5208e1a80998d9a1e2da55f1de9
F. Duncanh <fduncanh@gmail.com>
2024-06-19 12:24:29 -0400

    changes made for aspdk-public by @tishion

--- lib/hlsparse/write.h
+++ lib/hlsparse/write.h
@@ -10,18 +10,15 @@
 #include <stdarg.h>
 
 typedef struct page {
-    char *buffer;
-    int size;
-    char *cur;
-    struct page *next;
+  char *buffer;
+  int size;
+  char *cur;
+  struct page *next;
 } page_t;
 
-page_t* create_page(page_t *page);
-void free_page_root(page_t *root);
-
-page_t* write_to_page(page_t *page, const char *buffer, int size);
-page_t* pgprintf(page_t *page, const char *format, ...);
+page_t *create_page(page_t *page);
+page_t *write_to_page(page_t *page, const char *buffer, int size);
+page_t *pgprintf(page_t *page, const char *format, ...);
 void page_to_str(page_t *page, char **dest, int *dest_size);
-const char* find_relative_path(const char *path, const char *base);
 
 #endif // _WRITE_G
